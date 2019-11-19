2714f1cf9b9e326ddce002b0d374f521d136ff3f
Kelly K H <kh.waterloo@gmail.com>
2019-11-19 01:09:13 -0500

    split to 2 end points, Delete works now, will be based on ID

---
+++ server/endpoint/endptInventory.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const inventory = require('../data/inventory.json');
+
+
+// K: PATH /inventory/:inventoryId
+const inventDelete = (request, response) => {
+  console.log('request.params from inventory DELETE', request.params); // RETURN  { inventoryId: 'I5' }
+  const i = inventory.findIndex(info => info.id === request.params.inventoryId) // Searches in the array and find the index of the matching ID
+  if (i === -1) { // if not found send failure message
+    response.status(404).send('Inventory with the ID cannot be found')
+  } else { // If found at the index delete one item within inventory.json
+    inventory.splice(i, 1)
+    response.status(200).send(inventory) // Must return the new array as response
+  }
+  console.log('new inventory after inventory DELETE', inventory)
+}
+
+module.exports = {
+  // K: put function names here with commas
+  inventDelete
+}
