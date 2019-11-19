9f34092155c8dc5d72815935050963d38f20d527
Luca Lamanna <luca.lamanna@hotmail.com>
2019-11-19 15:13:36 -0500

    added a SCSS file to inventory list

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Inventory from './inventory-list/Inventory';
 
 function App() {
   return (
     <div className="App">
-        Hello
+        <Inventory />
     </div>
   );
 }
--- client/src/inventory-list/Inventory.js
+++ client/src/inventory-list/Inventory.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import './inventory.scss';
+
+//Importing Assets
+import searchIcon from '../assets/Icons/SVG/Icon-search.svg'
 
 export default class Inventory extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <section>
+                {/* <Header /> */}
+                <div className='inventory__container-for-title-search-bar'>
+                    <h1 className='inventory__page-title'>Inventory</h1>
+                    <div className="inventory__search-bar">
+                        <img className = "inventory__search-icon" src={searchIcon} alt="search icon" />
+                        <p className = "inventory__search-field">Search</p>
+                    </div>
+                </div>
+
+            </section>
         )
     }
 }
